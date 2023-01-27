76e4ecbef941487d977e7e23a94a47c8698ce246
rahim-jsr <rahimuddinjsr2620@gmail.com>
2023-01-27 20:48:48 +0600

    first commit

---
+++ array-and-conditions/array.js
@@ -0,0 +1,11 @@
+const friendsAge = [25, 25, 62, 52, 8, 96, 75];
+// console.log(friendsAge[5]);
+friendsAge[3] = 50;
+// console.log(friendsAge.length);
+// console.log(friendsAge.indexOf(50));
+
+console.log(friendsAge);
+friendsAge.push(85);
+console.log(friendsAge);
+const item = friendsAge.pop();
+console.log(item);
