8e070757bea81dc3b997723ee6876463e6510b1b
dev-rahim <rahimuddin5915@gmail.com>
2023-01-29 01:25:31 +0600

    recap- js concepts

---
+++ Recap-js-basic-concepts(variable-array0condition-loop)/recap-list.js
@@ -0,0 +1,132 @@
+/* 
+১. জাভাস্ক্রিপ্ট কি জিনিস এইটা কি জানো?
+JavaScript is a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else.
+
+২. জাভাস্ক্রিপ্ট কিভাবে কাজ করে সেটা কি জানো?
+
+In contrast, JavaScript has no compilation step. Instead, an interpreter in the browser reads over the JavaScript code, interprets each line, and runs it. More modern browsers use a technology known as Just-In-Time (JIT) compilation, which compiles JavaScript to executable bytecode just as it is about to run.
+
+৩. ভেরিয়েবল কি জিনিস?
+A JavaScript variable is simply a name of storage location. There are two types of variables in JavaScript : local variable and global variable.
+
+৪. ভেরিয়েবল কিভাবে ডিক্লেয়ার করে 
+let a= 52;
+const myName=Rahim;
+var studentName='karim'
+
+৫. ভেরিয়েবল এর মান কিভাবে চেইঞ্জ করে বা আপডেট করে। 
+studentName='Abdul Kairim'
+
+৬. কি কি ধরণের ভেরিয়েবল হয়। সেগুলা কি কি (হিন্টস: Numeric, String, Boolean)
+local variable
+global variable
+
+৭. জাভাস্ক্রিপ এ primitive and non primitive data types কি কি ? উদাহরণ হিসেবে বলো।
+
+Preemptive data types are
+    * string
+    * number
+    * boolean
+    * undefined
+    * null
+
+Non-primitive data types are
+
+    * Object
+    * Array
+    * RegEx
+
+
+৮. ভেরিয়েবল এর নাম কিভাবে কিভাবে ডিক্লেয়ার করতে হয়। কোন কোন জিনিস নাম এ লেখা যাবে না। অর্থাৎ Variable এর naming convention সম্পর্কে বলো।
+
+Naming Convention of Javascript Variables
+    * variable names are case-sensitive.
+    * The first character must be a letter or an underscore (_). You can't use a number as the first character.
+    * The rest of the variable name can include any letter, any number, or the underscore. You can't use any other characters, including spaces, symbols, and punctuation marks.
+    * There's no limit to the length of the variable name
+    * You can't use one of JavaScript's reserved words as a variable name
+   
+
+৯. দুইটা ভেরিয়েবল এর মধ্যে =, -, *, /, % কিভাবে করে? 
+a=11    for assign a variable value 
+a==b    for checking equality 
+c= a-b use for subtraction
+c=a*b  for multiplication
+c=a/b  for gatling divide
+d=a%^b use for get the remainder
+
+১০. শর্টহ্যান্ড গুলো জানতে হবে। বিশেষ করে +=, -=, *=, /= জানতে হবে।
+a+=10  added 10 with the value of a
+a-=10  substrate 10 from the value of a
+a *=10   multiply the valur of a with 10
+a /=10     divide the value of a by 10
+
+
+১১.. ++ এবং -- এর কাজ কি ? এইটা কি জানো। 
+a ++  add 1 with the value of a
+a --  substrate 1 fro the value of a
+
+১২ parseInt, parseFloat, toFixed এইগুলা কি করে? 
+
+parseInt  use for convert the value to Integer
+parseFloat use for convert the value to Float
+toFixed =>  it's use for fixed the number after flooting type data
+
+
+--------------
+
+১৩. Array কি জিনিস। এইটা কিভাবে কাজ করে? কিভাবে Array ডিক্লেয়ার করতে হয় 
+
+An array is a special variable, which can hold more than one value:
+array declaration 
+const numbers=[1,2,3,4,5,4,6] 
+
+১৪. array এর মধ্যে কয়টা উপাদান (element) আছে সেটা কিভাবে বের করে 
+
+array.length
+
+১৫. array এর উপাদান গুলা এর পজিশন ( index) কিভাবে কাজ করে। কত দিয়ে শুরু হয়। এবং মান কিভাবে চেইঞ্জ হয়। 
+
+১৬. কোন একটা উপাদানের index এর মান -১ বলতে কি বুঝায় 
+
+১৭. কিভাবে index দিয়ে কোন একটা array এর মধ্যে উপাদান এর মান খুঁজে বের করতে পারো 
+
+১৮. কিভাবে কোন একটা index পজিশন এ array এর উপাদান এর মান চেইঞ্জ করতে পারবে 
+
+১৯. একটা Array এর মধ্যে কোন একটা উপাদান এর মান তোমাকে দেয়া আছে এখন সেটার index তুমি কিভাবে খুঁজে বের করতে পারো?
+
+২০. ধরো, কোন একটা ইনডেক্স দিয়ে উপাদান খুঁজতে গেছো। কিন্তু সেটার মান না দিয়ে তোমাকে undefined দেখাচ্ছে। সেটা দেখে তুমি কি বুঝবে? (একটু গুগলে সার্চ দাও। আমরা কোর্স এ এইটা আলোচনা করিনি। তারপরেও চেষ্টা করে দেখো)
+
+২১. কোন একটা Array এর মধ্যে লাস্ট উপাদান হিসেবে কোন উপাদান হিসেবে যোগ করতে চাইলে কিভাৱে যোগ করবে। আবার Array থেকে শেষের উপাদান টা বের করে দিতে চাইলে কিভাবে বের করে দিবে
+
+২২. কোন একটা Array এর মধ্যে প্রথম উপাদান হিসেবে কোন উপাদান হিসেবে যোগ করতে চাইলে কিভাৱে যোগ করবে। আবার Array থেকে প্রথম উপাদান টা বের করে দিতে চাইলে কিভাবে বের করে দিবে
+
+২৩. তুলনা কিভাবে করতে হয়। এইগুলার মানে কি: >, <, ==, !=, <=, >=, ===, !==, &&, ।। 
+
+২৪. তোমার কাছে: ৮০০০০ টাকার বেশি হলে তুমি mac কিনবে, ৬০ টাকার বেশি হলে gaming ল্যাপটপ কিনবে, ৪০ হাজার টাকার বেশি হলে lenovo yoga কম্পিউটার কিনবে , ২০ হাজার টাকার বেশি হলে পুরান ল্যাপটপ কিনবে। না হয় তুমি মোবাইল দিয়ে কাজ চালাবে। 
+
+---------------------
+
+২৫. আসকে আমার মন ভালো নেই এই কথা ৩৯ বার আউটপুট হিসেবে দেখাও। 
+
+২৬. while লুপ কিভাবে কাজ করে 
+
+২৭. for লুপ কিভাবে কাজ করে 
+
+২৮. while লুপ এর মধ্যে লুপ ভেরিয়েবল চেইঞ্জ না করলে কি সমস্যা হয়। 
+
+২৯. একটা কোড লিখে ৫৮ থেকে ৯৮ পর্যন্ত যত সংখ্যা আছে সেগুলাকে দেখাও 
+
+৩০.একটা কোড লিখে ৪১২ থেকে ৪৫৬ পর্যন্ত যত জোর সংখ্যা আছে সেগুলাকে দেখাও  
+
+৩১. একটা কোড লিখে ৫৮১ থেকে ৬২৩ পর্যন্ত যত বিজোড় সংখ্যা আছে সেগুলাকে দেখাও 
+
+৩২.while আর for loop এর মধ্যে পার্থক্য কি 
+
+৩৩ তুমি এতদিন যা যা জিনিস শিখছো সেগুলার নাম দিয়ে একটা array বানাও। তারপর একটা for লুপ দিয়ে সেই array এর সব উপাদান কে আউটপুট হিসেবে দেখাও। 
+
+৩৪. তুমি এতদিন পর্যন্ত যে যে মডেলের মোবাইল ফোন ইউজ করেছো সেগুলার নাম দিয়ে একটা array বানাও। তারপর একটা while লুপ দিয়ে সেই array এর উপাদান গুলা একটা একটা করে আউটপুট হিসেবে দেখাও 
+
+৩৫. একটা ফর লুপ চালাও। ৩০ থেকে ৮৬ পর্যন্ত। আর এই লুপ ৪৪ এ গেলে ব্রেক করবে। সেই জিনিস কোড করে দেখাও 
+
+৩৬. তোমার যত বই আছে সেগুলার দাম নিয়ে একটা array লিখে ফেলো। যে বই গুলোর দাম ২০০ টাকার উপরে সেগুলাকে স্কিপ করবে। অর্থাৎ সেগুলাকে আউটপুট হিসেবে দেখাবে না। বাকিদের কে আউটপুট হিসেবে দেখাবে। দেখো করতে পারো কিনা।  */
