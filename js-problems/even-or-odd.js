192537e27bf0d5e2cc36cc91b2490eabe1dcd8b7
dev-rahim <rahimuddin5915@gmail.com>
2023-01-29 20:54:23 +0600

    added even-or-odd.js

---
+++ js-problems/even-or-odd.js
@@ -0,0 +1,8 @@
+let number = 56868.545;
+if (number % 2 == 0) {
+  console.log("The Number " + number + " is even");
+} else if (number % 2 != 0) {
+  console.log("The Number " + number + " is Odd");
+} else {
+  console.log("please enter a valid number");
+}
