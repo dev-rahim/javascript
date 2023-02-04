12b38402f24d4a3cd46173c02acc9f697f1a6fd5
dev-rahim <rahimuddin5915@gmail.com>
2023-02-04 12:50:32 +0600

    solvedmore js problems

--- js-pratices-day/concepts-pratcies.js
+++ js-pratices-day/concepts-pratcies.js
@@ -74,7 +74,7 @@ while (i <= 19) {
 }
 
 /* ৮. তোমাকে যদি বলা হয় তুমি একটা array ডিক্লেয়ার করবে। এবং সেটার মধ্যে কয়টা উপাদান আছে সেটা বের করবে হবে। সেই array এর এর মধ্যে চতুর্থ পজিশন এর উপাদান চেইঞ্জ করতে হবে। array এর মধ্যে দুইটা উপাদান যোগ করতে হবে। এবং একটা উপাদান কে বের করে দিতে হবে। তুমি কি সেটা করতে পারবে।  */
-const firends = ["sakil", "toybur", "Emon", "abdullah", "siam", "Taibur"];
+const firends = ["sakil", "toybur", "Emon", "abdullah", "siam", "Taibur",];
 console.log(firends);
 firends[3] = "Obaidullah";
 console.log(firends);
---
+++ js-pratices-day/more-problems.js
@@ -0,0 +1,81 @@
+// প্রব্লেম সলভিং চেকলিস্ট/সেলফ এসেসমেন্ট :
+
+/* ১. সিম্পল একটা ফাংশন লিখতে হবে। যেটার নাম হবে feetToInch এবং এই ফাংশন ইনপুট হিসেবে নিবে feet আর রিটার্ন করবে inch । অর্থাৎ এই ফাংশনকে কোন একটা ফিট বলে দিলে সে রিটার্ন হিসেবে বলে দিবে কত ইঞ্চি হয়।
+ */
+function feetToInch(feet) {
+  const inch = feet * 12;
+  return inch;
+}
+console.log(feetToInch(2));
+
+/* ২. একদম ফাংশন এর নাম হুবহু centimeterToMeter নাম দিয়ে একটা ফাংশন লিখবে। এই ফাংশনে ইনপুট হিসাবে কেউ সেন্টিমিটার দিবে আর সেই সেন্টিমিটার কে মিটার এ কনভার্ট করে রেজাল্ট রিটার্ন করবে।  */
+function centimeterToMeter(centimeter) {
+  const meter = centimeter * 0.01;
+  return meter;
+}
+console.log(centimeterToMeter(5000));
+
+/* ৩. আরেকটা ফাংশন লিখবে যেটার নাম লিখবে। যেই ফাংশনের নাম হবে paperRequirements এই ফাংশনের প্যারামিটার হিসেবে তিনটা প্যারামিটার হবে। প্রথম প্যারামিটার হবে তুমি প্রথম বই কত কপি ছাপাতে চাও। সেকেন্ড প্যারামিটার হবে তুমি সেকেন্ড বই কত কপি ছাপাতে চাও। আর থার্ড প্যারামিটার হবে তুমি থার্ড বই কত কপি ছাপাতে চাও। অর্থাৎ কোন বই এর কত কপি ছাপানো হবে সেটাই প্যারামিটার হিসেবে নিবে। 
+
+এইবার ভালো করে খেয়াল করো। 
+
+প্রথম বই ছাপানোর জন্য পৃষ্ঠা লাগবে ১০০ টা 
+
+সেকেন্ড বই ছাপানোর জন্য পৃষ্ঠা লাগবে ২০০ টা 
+
+তৃতীয় বই ছাপানোর জন্য পৃষ্ঠা লাগবে ৩০০ টা 
+
+এখন তোমার কাজ হচ্ছে paperRequirements নামক ফাংশন লিখে ফেলা যাতে। সেই ফাংশনকে কল করে কোন বই এর কত কপি লাগবে বলে দিবে প্যারামিটার হিসেবে। আর ফাংশন হিসাব করে বলে দিবে তোমার সর্বমোট কতপৃষ্ঠা কাগজ লাগবে। 
+
+উত্তর হিসেবে সংখ্যা রিটার্ন করবে। . */
+
+function paperRequirements(
+  firstBookPicesToPrint,
+  secondBookPicesToPrint,
+  thirdBookPicesToPrint
+) {
+  const firstBookTotalParer = firstBookPicesToPrint * 100;
+  const secondBookTotalParer = secondBookPicesToPrint * 200;
+  const thirdBookTotalParer = thirdBookPicesToPrint * 300;
+  const totalpaperRequire =
+    firstBookPicesToPrint + secondBookPicesToPrint + thirdBookPicesToPrint;
+  return totalpaperRequire;
+}
+
+/* ৪. একটা ফাংশন লিখবে। এই ফাংশনের নাম হবে bestFriend তারপর সেই ফাংশনে ইনপুট প্যারামিটার হিসেবে একটা array নিবে। সেই array এর মধ্যে তোমার সব ফ্রেন্ডের নাম থাকবে। এখন তোমার কাজ হচ্ছে যে ফ্রেন্ড এর নাম সবচেয়ে বড় সেই ফ্রেন্ড এর নাম রিটার্ন করে দেয়া। এই ক্ষেত্রে তুমি নামটা অর্থাৎ ফ্রেন্ডের নাম (স্ট্রিং) রিটার্ন করতে হবে। */
+function bestFriend(friends) {
+  let bestFriend = friends[0];
+  for (const friend of friends) {
+    if (firends.length > bestFriend.length) {
+      bestFriend = friend;
+    }
+  }
+  return bestFriend;
+}
+const firends = [
+  "sakil",
+  "toybur",
+  "Emon",
+  "abdullah",
+  "siam",
+  "Taibur",
+  "Borson",
+  "Plabon",
+];
+console.log(bestFriend(firends));
+/* ৫. এইটা একটু ট্রিকি হতে পারে। একটা array এর মধ্যে অনেকগুলা সংখ্যা থাকবে। তোমার কাজ হচ্ছে সংখ্যা গুলা একটার পর একটা করে চেক করা। এবং সংখ্যা গুলা যদি পজিটিভ সংখ্যা হয়। অর্থাৎ শূন্য বা শূন্যের চাইতে বড় হয় তাহলে সেগুলাকে কোন একটা array এর মধ্যে রাখবে। আর যদি নেগেটিভ সংখ্যা হয়। তাহলে লুপটা স্টপ করে দিবে। এবং লুপ বন্ধ করার আগ পর্যন্ত যতগুলা পজিটিভ সংখ্যা পাওয়া গেছে। সেগুলা রিটার্ন করে দিবে।  */
+
+function filterPositive(numbers) {
+  const posetive = [];
+  for (const number of numbers) {
+    if (number < 0) {
+      break;
+    } else {
+      posetive.push(number);
+    }
+  }
+  return posetive;
+}
+const numbers = [12, 45, 52, 65, 12, -121, 52, 65, 41, 25, 53, 42];
+
+console.log(filterPositive(numbers));
